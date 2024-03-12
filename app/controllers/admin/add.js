4cb516b220466af560afd47e225de7471d1e4560
Jéssica <jessica@systemar.com.br>
2024-03-12 17:23:54 -0300

    começando rotas de add, edit, search...

--- app/controllers/admin/add.js
+++ app/controllers/admin/add.js
@@ -1,39 +1,48 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default class AdminAddController extends Controller {
+  @service store;
+
   @action
   addProduct() {
-    let title = this.title;
-    let desc = this.desc;
-    let price = this.price;
 
+    let desc = this.model.desc;
+    let price = this.model.price;
     let e = document.getElementById('category');
     let category = e.options[e.selectedIndex].value;
+    let imglink = this.model.imglink;
 
-    let imglink = this.imglink;
 
-    //exemplo de integração com o banco
-    var newproduct = this.store.createRecord('product', {
-      //
-      product_title: title,
+    let newProduct = this.store.createRecord('product', {
+      productTitle: this.productTitle,
       desc: desc,
       price: price,
       category_id: category,
       imglink: imglink,
     });
 
-    newproduct.save();
+    newProduct.save();
+
+    function setInput(tipo, valor) {
+      set(this.model, tipo, valor);
+    }
+
+    setInput();
 
     alert('Product Added');
 
     // Clear
     this.setProperties({
-      title: '',
+      productTitle: '',
       desc: '',
       price: '',
       imglink: '',
     });
     e.value = '';
   }
+
+
 }
